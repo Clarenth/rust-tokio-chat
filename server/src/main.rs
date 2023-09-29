0b736b9e15b76844633d0baa505091adffd7a55b
Clarenth <standadit.dev@protonmail.com>
2023-09-29 15:03:17 -0400

    Chat now excludes echoing from the message sender.

--- server/src/main.rs
+++ server/src/main.rs
@@ -16,12 +16,12 @@ async fn main()
   let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
   // setup a broadcast channel to allow producers and consumers to share between each other
-  let (tx, _rx) = broadcast::channel::<String>(10);
+  let (tx, _rx) = broadcast::channel(10);
 
   loop 
   {
     // assign the listener to a socket and address. _ <- underscore ignores warnings, errors, for now (like in Go)
-    let (mut socket, _addr) = listener.accept().await.unwrap();
+    let (mut socket, addr) = listener.accept().await.unwrap();
 
     let tx = tx.clone();
     let mut rx = tx.subscribe();
@@ -42,27 +42,30 @@ async fn main()
 
       loop 
       {
-          tokio::select! 
+        tokio::select! 
+        {
+          result = reader.read_line(&mut line) => 
           {
-            result = reader.read_line(&mut line) => 
-            {
-              if result.unwrap() == 0 
-              { // if we have 0 bytes incoming we have reached the end of the file 
-                break;
-              }
-
-              tx.send(line.clone()).unwrap();
-              // line.clear() clears variable 'line'. read_line appends the next stream line
-              // to whatever has comes through. We must clear the string outselves.
-              // There is a practical reason to keep this (ex: reading in a file line by line)
-              line.clear();
+            if result.unwrap() == 0 
+            { // if we have 0 bytes incoming we have reached the end of the file 
+              break;
             }
-            result = rx.recv() => 
+            tx.send((line.clone(), addr)).unwrap();
+            // line.clear() clears variable 'line'. read_line appends the next stream line
+            // to whatever has comes through. We must clear the string outselves.
+            // There is a practical reason to keep this (ex: reading in a file line by line)
+            line.clear();
+          }
+          result = rx.recv() => 
+          {
+            let (msg, other_addr) = result.unwrap();
+
+            if addr != other_addr 
             {
-              let msg = result.unwrap();
               writer.write_all(msg.as_bytes()).await.unwrap();
             }
           }
+        }
       }
     });
   }
